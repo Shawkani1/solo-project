61e0d0f5498ad174e0a4c4a209d062e7be433365
Abdirahman Mohamed <Abdimo1324@gmail.com>
2025-02-26 17:46:10 -0600

    Changed the about page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -116,31 +116,13 @@ function App() {
             element={
               <div className="about-container">
                 <h2 className="about-title">About Page</h2>
-                <p className="about-text">
-                  Intelligence doesn't seem like an aspect of personal character, and it isn't.
-                  Coincidentally, great intelligence is only loosely connected to being a good programmer.
-                </p>
-                <p className="about-text">
-                  What? You don't have to be superintelligent?
-                </p>
-                <p className="about-text">
-                  No, you don't. Nobody is really smart enough to program computers.
-                  Fully understanding an average program requires an almost limitless capacity
-                  to absorb details and an equal capacity to comprehend them all at the same time.
-                  The way you focus your intelligence is more important than how much intelligence you have…
-                </p>
+                
+             
                 <p className="about-quote">
-                  …most of programming is an attempt to compensate for the strictly limited size of our skulls.
-                  The people who are the best programmers are the people who realize how small their brains are.
-                  They are humble. The people who are the worst at programming are the people who refuse to
-                  accept the fact that their brains aren't equal to the task.
-                  Their egos keep them from being great programmers.
-                  The more you learn to compensate for your small brain, the better a programmer you'll be.
-                  The more humble you are, the faster you'll improve.
+                  
+Thank you for Listening and watching my presentation. The technologies I used were React, Node.js, Express, Tailwind CSS and PostgreSQL to build this app.
                 </p>
-                <p className="about-citation">
-                  --From Steve McConnell's <em>Code Complete</em>.
-                </p>
+               
                 {user?.id && (
                   <Link to="/" className="back-to-donors-button">
                     Back to Donor List
